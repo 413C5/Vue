61a4f384ee7b942b22a5e03475984f98d0e88938
Alejandro González Zárate <124840787+413C5@users.noreply.github.com>
2024-06-05 13:03:08 -0600

    Reactive variables
    Reactive variables were used to change a value

--- 02-intro-vue/app.jsx
+++ 02-intro-vue/app.jsx
@@ -3,8 +3,21 @@ const { createApp, ref } = Vue;
 const app = createApp({
 	template: `
     <h1>Hola mundo</h1>
-    <p>Desde app.jsx</p>
+    <h2>{{message}}</h2>
+    <p>-{{author}}</p>
     `,
+
+	setup() {
+		const message = ref("I'm batman");
+		const author = ref('Bruce Wayne');
+
+		setTimeout(() => {
+			message.value = 'Soy Goku';
+			author.value = 'Goku';
+		}, 1000);
+
+		return { message,author };
+	},
 });
 
 app.mount('#myApp');
