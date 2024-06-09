f01234eed38b8b5bbb48bdba48a85bba7c7db669
Alejandro González Zárate <124840787+413C5@users.noreply.github.com>
2024-06-08 21:23:46 -0600

    Created a traditional component

--- 03-indecision-app/src/App.vue
+++ 03-indecision-app/src/App.vue
@@ -2,8 +2,10 @@
   <h1>Mi primera app</h1>
   <hr />
   <MyCounter :value="10"/>
+  <MyCounterScript :value="10"/>
 </template>
 
 <script lang="ts" setup>
 import MyCounter from './components/MyCounter.vue';
+import MyCounterScript from './components/MyCounterScript.vue';
 </script>
---
+++ 03-indecision-app/src/components/MyCounterScript.vue
@@ -0,0 +1,41 @@
+<template>
+  <section>
+    <h3>Counter {{ counter }}</h3>
+    <h3>Square {{ squareCounter }}</h3>
+
+    <div>
+      <button @click="increaseValue">+1</button>
+      <button @click="decreaseValue">-1</button>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { computed, ref, defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    value: { type: Number, required: true }
+  },
+
+  setup(props) {
+    const counter = ref(props.value);
+    const squareCounter = computed(() => counter.value * counter.value);
+
+    const increaseValue = () => {
+      counter.value++;
+    };
+
+    const decreaseValue = () => {
+      counter.value--;
+    };
+
+    return {
+      counter,
+      squareCounter,
+      increaseValue,
+      decreaseValue
+    };
+  }
+});
+</script>
